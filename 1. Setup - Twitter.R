9cd4afc94ef6508e828dd0e27a6bf89c40018e48
Josep Curto <josep.curto@gmail.com>
2014-05-23 10:11:17 +0100

    Adding setup files for Twitter and Facebook
    The files include the initial packages that help to analyse social data.

---
+++ 1. Setup - Twitter.R
@@ -0,0 +1,28 @@
+###############################################################################
+## Title: Social Analytics for Twitter
+## Version: 0.1
+## Autor: Josep Curto
+## Description: set of libraries that can be used to analyse Twitter data
+###############################################################################
+
+# Package installation
+install.packages(c("devtools", "rjson", "bit64", "httr"))
+library(devtools)
+install_github("httr")
+install_github("twitteR", username="geoffjentry")
+install.packages("plyr")
+install.packages("stringr")
+install.packages("tm")
+install.packages("maps")
+install.packages("ggplot2")
+install.packages("SnowballC")
+install.packages("wordcloud")
+
+# Package load
+library(twitteR)
+library(plyr)
+library(stringr)
+library(tm)
+library(ggplot2)
+library(SnowballC)
+library(wordcloud)
